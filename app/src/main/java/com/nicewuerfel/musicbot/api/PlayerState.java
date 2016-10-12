892604b8fb496f5503d07fed5d0c2f7181e1d52d
Björn Petersen <nicewuerfel@gmail.com>
2016-10-12 19:23:41 +0200

    Optimize imports

--- app/src/main/java/com/nicewuerfel/musicbot/api/PlayerState.java
+++ app/src/main/java/com/nicewuerfel/musicbot/api/PlayerState.java
@@ -4,7 +4,6 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.util.AsyncListUtil;
 
 import java.util.Collections;
 import java.util.List;
