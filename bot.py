27f8b08a94279ca1e03769ae33bf38f99295e143
Björn Petersen <nicewuerfel@gmail.com>
2016-10-14 02:01:16 +0200

    Add artifact upload bot

---
+++ bot.py
@@ -0,0 +1,15 @@
+import os
+
+from telegram.ext import Updater
+
+token = os.environ['token']
+chat_id = "-154311545"
+updater = Updater(token=token)
+bot = updater.bot
+artifacts_dir = os.environ['CIRCLE_ARTIFACTS']
+filename = "AndroidMusicBot.apk"
+file_path = os.path.join(artifacts_dir, filename)
+if os.path.isfile(file_path):
+    with open(file_path, 'rb') as bot_file:
+        bot.send_document(chat_id=chat_id, document=bot_file, filename=filename)
+updater.stop()
